b6ad9cdabc24ff4f1c5ed03a5e6987e10cd1bb08
seafinn <ciara@finnerty.ie>
2025-11-11 23:49:18 +0000

    Created drone.c and drone.h

---
+++ drone.c
@@ -0,0 +1,14 @@
+/*
+ *  Handles structs relating to data needed to track velocity,
+ *  starting depot, delivery location, spawn time, despawn time,
+ *  collision check, distance travelled and its mean velocity.
+ */
+
+#include "drone.h"
+
+#include <stdio.h>
+
+void helloDrone()
+{
+    printf("Hello Drone!\n");
+}
---
+++ drone.h
@@ -0,0 +1,10 @@
+//
+// Created by ciara on 11/11/2025.
+//
+
+#ifndef CDRONE_DRONE_H
+#define CDRONE_DRONE_H
+
+void helloDrone();
+
+#endif //CDRONE_DRONE_H
