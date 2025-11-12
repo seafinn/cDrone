14b8eb62ac75cb22d0738bd8c170bfae45357773
seafinn <ciara@finnerty.ie>
2025-11-12 00:26:36 +0000

    Added Drone and vector structs to drone.h

--- drone.h
+++ drone.h
@@ -5,6 +5,28 @@
 #ifndef CDRONE_DRONE_H
 #define CDRONE_DRONE_H
 
+//  3D Vector to determine drone positions
+typedef struct
+{
+    double x,y,z;
+} droneVector;
+
+static inline droneVector droneVecAdd(droneVector a, droneVector b)
+{
+    return (droneVector){a.x+b.x, a.y+b.y, a.z+b.z };
+}
+
+static inline droneVector droneVecSubtract(droneVector a, droneVector b)
+{
+    return (droneVector){a.x-b.x, a.y-b.y, a.z-b.z };
+}
+
+typedef struct
+{
+    droneVector startPos, endPos, currentPos, vel;
+    double spawnTime, despawnTime, distTravelled, meanVel;
+    bool hasCollided, isActive;
+} Drone;
 void helloDrone();
 
 #endif //CDRONE_DRONE_H
