a83cfda8c6671683ff5c28eabe8a2ca3db521452
seafinn <ciara@finnerty.ie>
2025-11-12 00:27:41 +0000

    CMake changes

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "drone.h"
 int main() {
     // printf() displays the string inside quotation
-    printf("Hello, World!");
+    printf("Hello, World!\n");
+    helloDrone();
     return 0;
 }
